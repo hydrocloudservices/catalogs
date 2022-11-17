4719639063ae54ed0deae49eb0409fcfdc2651c9
Sebastien Langlois <42305216+sebastienlanglois@users.noreply.github.com>
2022-11-17 15:29:57 -0500

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -30,6 +30,6 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx-pangeo-theme'
 html_static_path = ['_static']
 html_style = 'custom.css'
