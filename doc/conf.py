71b7f9567b0d57896fa2bbc3c54105e4c85ca4a2
sebastienlanglois <sebastien.langlois@polymtl.ca>
2022-11-18 12:33:47 -0500

    Rename project

--- doc/conf.py
+++ doc/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'catalogs'
+project = 'eo-catalog'
 copyright = '2022, Sebastien Langlois'
 author = 'Sebastien Langlois'
 release = '0.0.1'
